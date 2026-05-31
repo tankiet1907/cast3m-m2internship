e2444ec8c36f5765f72b45848776ab4868f71327
Your Name <you@example.com>
2026-06-01 03:34:54 +0700

    .

--- 04.OpenTURNS/Test_02/openturns-kalifa.py
+++ 04.OpenTURNS/Test_02/openturns-kalifa.py
@@ -4,3 +4,6 @@ import subprocess
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+
+working_dir = r"D:\cast3m-m2internship\04.OpenTURNS\Test_02"
+csv_dir = r"D:\cast3m-m2internship\04.OpenTURNS\Test_02\CSV"
